863ab589dab253ba4208326c882cf41157b97e37
eemeli <eemeli.koikkalainen@outlook.com>
2026-07-12 10:19:50 +0300

    make log app an http server

--- log_output/cmd/main.go
+++ log_output/cmd/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 func main() {
-	ticker := time.NewTicker(time.Second * 5)
-	random := uuid.New()
-	var currentTime time.Time
+	PORT := "3456"
+	if p := os.Getenv("PORT"); p != "" {
+		PORT = p
+	}
 
-	for {
-		currentTime = <-ticker.C
-		fmt.Printf("%s: %s\n", currentTime.Format("2006-01-02T15:04:05.000Z"), random)
+	http.HandleFunc("/", homeHandler())
+
+	addr := fmt.Sprintf(":%s", PORT)
+	fmt.Printf("Server started in port %s\n", PORT)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func homeHandler() http.HandlerFunc {
+	random := uuid.New()
+	return func(w http.ResponseWriter, _ *http.Request) {
+		_, err := fmt.Fprintf(w, "%s: %s", time.Now().Format("2006-01-02T15:04:05.000Z"), random)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
